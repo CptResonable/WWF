8ea7abf6c750cbb6b14fe7cc9965c764273caad1
CptResonable <adolfssonalbin.94@gmail.com>
2022-05-19 17:20:13 +0100

    Canged locomotion to move at correct speed

--- WWF_S/Assets/Scripts/Locomotion.cs
+++ WWF_S/Assets/Scripts/Locomotion.cs
@@ -5,9 +5,13 @@ using VacuumBreather;
 
 [System.Serializable]
 public class Locomotion {
-    private const float WALK_SPEED = 10;
-    private const float JOG_SPEED = 14;
-    private const float SPRINT_SPEED = 17;
+    //private const float WALK_SPEED = 10;
+    //private const float JOG_SPEED = 14;
+    //private const float SPRINT_SPEED = 17;
+    private const float WALK_SPEED = 3;
+    private const float JOG_SPEED = 5;
+    private const float SPRINT_SPEED = 6.5f;
+
 
     private CharacterLS character;
 
@@ -92,6 +96,7 @@ public class Locomotion {
         }
     }
 
+
     private void Move() {
 
         // Inititate PIDs.
@@ -100,7 +105,7 @@ public class Locomotion {
 
         Vector3 xzForward = new Vector3(character.tTargetYaw.forward.x, 0, character.tTargetYaw.forward.z).normalized;
         Vector3 xzRight = new Vector3(character.tTargetYaw.right.x, 0, character.tTargetYaw.right.z).normalized;
-        Vector3 vecMove = (character.rbMain.transform.forward * character.input.vecMoveXZ.y + character.rbMain.transform.transform.right * character.input.vecMoveXZ.x).normalized * moveSpeed * Time.deltaTime;
+        Vector3 vecMove = (character.rbMain.transform.forward * character.input.vecMoveXZ.y + character.rbMain.transform.transform.right * character.input.vecMoveXZ.x);
 
         // I use velocity to target velocity for PID error.
         Vector3 error = vecMove * moveSpeed - character.rbMain.velocity;
@@ -112,6 +117,28 @@ public class Locomotion {
 
         character.rbMain.AddForce(moveForce);
     }
+
+
+    //private void Move() {
+
+    //    // Inititate PIDs.
+    //    PidController pid_x = new PidController(pidValues_move.x, pidValues_move.y, pidValues_move.z);
+    //    PidController pid_z = new PidController(pidValues_move.x, pidValues_move.y, pidValues_move.z);
+
+    //    Vector3 xzForward = new Vector3(character.tTargetYaw.forward.x, 0, character.tTargetYaw.forward.z).normalized;
+    //    Vector3 xzRight = new Vector3(character.tTargetYaw.right.x, 0, character.tTargetYaw.right.z).normalized;
+    //    Vector3 vecMove = (character.rbMain.transform.forward * character.input.vecMoveXZ.y + character.rbMain.transform.transform.right * character.input.vecMoveXZ.x).normalized * moveSpeed * Time.deltaTime;
+
+    //    // I use velocity to target velocity for PID error.
+    //    Vector3 error = vecMove * moveSpeed - character.rbMain.velocity;
+
+    //    // Do PID calculations to get move forces.
+    //    Vector3 moveForce = Vector3.zero;
+    //    moveForce.x = pid_x.ComputeOutput(error.x, character.telemetry.acceleration.x, Time.deltaTime);
+    //    moveForce.z = pid_z.ComputeOutput(error.z, character.telemetry.acceleration.z, Time.deltaTime);
+
+    //    character.rbMain.AddForce(moveForce);
+    //}
 
     private void FrictionForce() {
         targetMoveVector = character.rbMain.transform.TransformVector(character.input.v3vecMoveXZ);
